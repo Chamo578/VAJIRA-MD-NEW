2fe4b523d1dc231ba47033932b1ec5c4aabd9177
Chamo578 <manelmax031@gmail.com>
2024-09-25 21:54:11 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '94701992015' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=PcNgBTbI#6zDlpTA4DHsGzMcPlAcfR8Xd6k2r8YwyBjzbnQJmtU0 ' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=uQ1xhbBI#QvH6EQJyVK_4_wpI-Xj0vLzJf2Ag-JSBCMhWcBpVB8c' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
